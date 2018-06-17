c88de78598b212d614efa4403c86def9b3ed448c
corganfuzz <gruizcavero@uh.edu>
2018-06-17 12:39:18 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import { SuiModule } from 'ng2-semantic-ui';
+import {FormsModule,
+        ReactiveFormsModule } from '@angular/forms';
+import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
+import { DemoFormWithEventsComponent } from './demo-form-with-events/demo-form-with-events.component';
+import { DemoFormWithValidationsExplicitComponent } from './demo-form-with-validations-explicit/demo-form-with-validations-explicit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoFormSkuComponent,
+    DemoFormSkuWithBuilderComponent,
+    DemoFormWithEventsComponent,
+    DemoFormWithValidationsExplicitComponent,
+    // FormsDemoApp,
+    // DemoFormSkuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
